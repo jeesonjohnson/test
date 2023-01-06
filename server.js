83bb32921fcf9d6f8cfbe18b3722c79a0fef4c26
Jeeson Johnson <jeesonjohnson@outlook.com>
2023-01-06 09:21:11 +0000

    Feat: Added database connections

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+app.get('*', (req, res) => {
+
+  res.send(`${req.hostname},${req.path}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
